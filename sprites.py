a66edc9cdde16e3d8322e2e0092a425e38e344f4
Pifanza <danya.epifantsev@gmail.com>
2020-11-15 17:21:48 +0300

    Добавил класс бот и создал 2х роботов

---
+++ sprites.py
@@ -0,0 +1,14 @@
+import pygame
+
+class Bot(self, name, age, seria, wight):
+    self.bot_name = name
+    self.bot_age = age
+    self.id = seria
+    self.wight = wight
+    def spec(self):
+        print('Имя:', self.bot_name)
+        print(f'возраст/вес: {self.bot_age}/{self.wight}')
+        print('ID:', self.id)
+r1 = Bot(robby, 2, 2565, 45)
+r2 = Bot(rudy, 2, '2560-s', 44.5)
+
