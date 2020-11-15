73234e23cecacf5a8c2a3f069ce40a4a790c35a6
Pifanza <danya.epifantsev@gmail.com>
2020-11-15 17:34:29 +0300

    Убрал модуль pygame, наладил работу конструктора

--- sprites.py
+++ sprites.py
@@ -1,14 +1,15 @@
-import pygame
-
-class Bot(self, name, age, seria, wight):
-    self.bot_name = name
-    self.bot_age = age
-    self.id = seria
-    self.wight = wight
+class Bot:
+    def __init__(self, name, age, seria, wight):
+        self.bot_name = name
+        self.bot_age = age
+        self.id = seria
+        self.wight = wight
     def spec(self):
         print('Имя:', self.bot_name)
         print(f'возраст/вес: {self.bot_age}/{self.wight}')
         print('ID:', self.id)
-r1 = Bot(robby, 2, 2565, 45)
-r2 = Bot(rudy, 2, '2560-s', 44.5)
+r1 = Bot('robby', 2, 2565, 45)
+r2 = Bot('rudy', 2, '2560-s', 44.5)
+r1.spec()
+r2.spec()
 
